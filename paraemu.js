5305c64d48d14af253446c3a066fd0ad1eabc93b
brchen777 <eric790620@gmail.com>
2018-08-10 17:43:32 +0800

    Fix event emitter

--- paraemu.js
+++ paraemu.js
@@ -6,75 +6,70 @@
     const events = require('events');
     const cluster = require('cluster');
 
-    const eventEmitter = new events.EventEmitter();
     const procQueue = [];
-    const exports = {
-        on: (event, cb) => {
-            console.log('on');
-            eventEmitter.on(event, cb);
-        },
-        off: (event, cb) => {
-            console.log('off');
-            eventEmitter.removeListener(event, cb);
-        },
-        emit: (event, ...args) => {
-            console.log('emit');
-            // send from worker to master
-            eventEmitter.send({ event, args });
-        }
+    const exports = new events.EventEmitter();
+    const oriEmmiter = exports.emit;
+
+    exports.emit = (event, ...args) => {
+        console.log('emit');
+        // send from worker to master
+        process.send({ event, args });
     };
     module.exports = exports;
 
-    const workerEventHandler = (worker) => {
-        worker.on('online', () => {
-            console.log('online');
-            procQueue.push(worker);
-        });
+    if (cluster.isMaster) {
+        const eventHandler = (worker) => {
+            worker.on('online', () => {
+                console.log('online');
+                procQueue.push(worker);
+            });
 
-        // If process disconnected, remove worker in procQueue
-        worker.on('disconnect', () => {
-            console.log('disconnect');
-            const removeIdx = procQueue.indexOf(worker);
-            if (removeIdx !== -1) {
-                procQueue.splice(removeIdx, 1);
-            }
-        });
+            // If process disconnected, remove worker in procQueue
+            worker.on('disconnect', () => {
+                console.log('disconnect');
+                const removeIdx = procQueue.indexOf(worker);
+                if (removeIdx !== -1) {
+                    procQueue.splice(removeIdx, 1);
+                }
+            });
+        };
 
-        worker.on('message', (msg) => {
+        if (process.isCLI) {
+            const relFilePath = process.argv[0];
+            const absFilePath = path.resolve(relFilePath);
+            const opts = process.argv.slice(1);
+
+            // parse config file
+            const cfg = JSON.parse(fs.readFileSync(absFilePath, 'utf8'));
+            const { process: cfgProcs } = cfg;
+
+            cfgProcs.forEach((cfgProc) => {
+                // get absolute directory path
+                const relProcPath = cfgProc['working-dir'];
+                const absProcPath = path.resolve(relProcPath);
+
+                cluster.setupMaster({
+                    cwd: absProcPath,
+                    exec: cfgProc.script,
+                    args: cfgProc.args || []
+                });
+                const worker = cluster.fork();
+                eventHandler(worker);
+            });
+
+            // master receive message form worker
+            cluster.on('message', (worker, msg) => {
+                procQueue.forEach((proc) => {
+                    proc.send(msg);
+                });
+            });
+        }
+    }
+    else {
+        process.on('message', (msg) => {
             console.log('message');
             const { event, args } = msg;
-            eventEmitter.emit(event, ...args);
-        });
-    };
-
-    if (process.isCLI) {
-        const relFilePath = process.argv[0];
-        const absFilePath = path.resolve(relFilePath);
-        const opts = process.argv.slice(1);
-
-        // parse config file
-        const cfg = JSON.parse(fs.readFileSync(absFilePath, 'utf8'));
-        const { process: cfgProcs } = cfg;
-
-        cfgProcs.forEach((cfgProc) => {
-            // get absolute directory path
-            const relProcPath = cfgProc['working-dir'];
-            const absProcPath = path.resolve(relProcPath);
-
-            cluster.setupMaster({
-                cwd: absProcPath,
-                exec: cfgProc.script,
-                args: cfgProc.args || []
-            });
-            const worker = cluster.fork();
-            workerEventHandler(worker);
-        });
-
-        // master receive message form worker
-        cluster.on('message', (worker, msg) => {
-            procQueue.forEach((proc) => {
-                proc.send(msg);
-            });
+            oriEmmiter.call(exports, event, ...args);
         });
     }
 })();
