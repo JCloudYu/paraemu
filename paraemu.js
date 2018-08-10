fd1d08872282177233b029b2ef9afe3b59a3fc51
brchen777 <eric790620@gmail.com>
2018-08-10 16:39:56 +0800

    Fix send message error

--- paraemu.js
+++ paraemu.js
@@ -1,31 +1,39 @@
 (async () => {
     'use strict';
 
+    const fs = require('fs');
+    const path = require('path');
     const events = require('events');
+    const cluster = require('cluster');
+
     const eventEmitter = new events.EventEmitter();
     const procQueue = [];
     const exports = {
         on: (event, cb) => {
+            console.log('on');
             eventEmitter.on(event, cb);
         },
         off: (event, cb) => {
+            console.log('off');
             eventEmitter.removeListener(event, cb);
         },
         emit: (event, ...args) => {
-            procQueue.forEach((proc) => {
-                proc.send({ event, args });
-            });
+            console.log('emit');
+            // send from worker to master
+            eventEmitter.send({ event, args });
         }
     };
     module.exports = exports;
 
     const workerEventHandler = (worker) => {
-        worker.on('fork', () => {
+        worker.on('online', () => {
+            console.log('online');
             procQueue.push(worker);
         });
 
-        // if process disconnected, remove worker in procQueue
+        // If process disconnected, remove worker in procQueue
         worker.on('disconnect', () => {
+            console.log('disconnect');
             const removeIdx = procQueue.indexOf(worker);
             if (removeIdx !== -1) {
                 procQueue.splice(removeIdx, 1);
@@ -33,14 +41,12 @@
         });
 
         worker.on('message', (msg) => {
+            console.log('message');
             const { event, args } = msg;
             eventEmitter.emit(event, ...args);
         });
     };
 
-    const fs = require('fs');
-    const path = require('path');
-    const cluster = require('cluster');
     if (process.isCLI) {
         const relFilePath = process.argv[0];
         const absFilePath = path.resolve(relFilePath);
@@ -62,6 +68,13 @@
             });
             const worker = cluster.fork();
             workerEventHandler(worker);
+        });
+
+        // master receive message form worker
+        cluster.on('message', (worker, msg) => {
+            procQueue.forEach((proc) => {
+                proc.send(msg);
+            });
         });
     }
 })();
